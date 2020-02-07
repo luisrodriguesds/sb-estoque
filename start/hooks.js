c05a210cf0e0ecb976c8865346ddd1b2d0bc516c
Luis Rodrigues <luisitaloar@gmail.com>
2020-02-07 18:05:44 -0300

    Revert "Corrigindo URLS"
    This reverts commit 9b4a88033e7dcd402ab5993163505375a21d8273.

--- start/hooks.js
+++ start/hooks.js
@@ -4,21 +4,18 @@ hooks.after.providersBooted(() => {
 
     const Exception = use('Exception')
     const Env       = use('Env')
+    const slash     = use('Env').get('APP_URL_SLASH')
     const View      = use('View')
   
     Exception.handle('InvalidSessionException', async (error, {response}) =>{
-        return response.redirect(Env.get('APP_URL_PROD')+'/login')
+        return response.redirect(slash+'/login')
     })
 
     Exception.handle('HttpException', async (error, {response}) =>{
-        return response.redirect(Env.get('APP_URL_PROD')+'/dashboard')
+        return response.redirect(slash+'/dashboard')
     })
 
     View.global('URLBASE', () => {
         return Env.get('APP_URL_PROD')
-    })
-
-    View.global('SLASH', () => {
-        return Env.get('SLASH')
     })
 })
