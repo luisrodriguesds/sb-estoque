9b4a88033e7dcd402ab5993163505375a21d8273
Luis Rodrigues <luisitaloar@gmail.com>
2020-02-07 18:04:27 -0300

    Corrigindo URLS

--- start/hooks.js
+++ start/hooks.js
@@ -4,18 +4,21 @@ hooks.after.providersBooted(() => {
 
     const Exception = use('Exception')
     const Env       = use('Env')
-    const slash     = use('Env').get('APP_URL_SLASH')
     const View      = use('View')
   
     Exception.handle('InvalidSessionException', async (error, {response}) =>{
-        return response.redirect(slash+'/login')
+        return response.redirect(Env.get('APP_URL_PROD')+'/login')
     })
 
     Exception.handle('HttpException', async (error, {response}) =>{
-        return response.redirect(slash+'/dashboard')
+        return response.redirect(Env.get('APP_URL_PROD')+'/dashboard')
     })
 
     View.global('URLBASE', () => {
         return Env.get('APP_URL_PROD')
+    })
+
+    View.global('SLASH', () => {
+        return Env.get('SLASH')
     })
 })
