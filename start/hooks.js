07a49d7fff4e5f5ef498cfc4550e4b27a6797bff
Luis Rodrigues <luisitaloar@gmail.com>
2020-02-07 17:52:19 -0300

    Atualizando slash 2

--- start/hooks.js
+++ start/hooks.js
@@ -3,15 +3,16 @@ const { hooks } = use('@adonisjs/ignitor')
 hooks.after.providersBooted(() => {
 
     const Exception = use('Exception')
-    const Env = use('Env')
-    const View = use('View')
+    const Env       = use('Env')
+    const slash     = use('Env').get('APP_URL_SLASH')
+    const View      = use('View')
   
     Exception.handle('InvalidSessionException', async (error, {response}) =>{
-        return response.redirect('/login')
+        return response.redirect(slash+'/login')
     })
 
     Exception.handle('HttpException', async (error, {response}) =>{
-        return response.redirect('/dashboard')
+        return response.redirect(slash+'/dashboard')
     })
 
     View.global('URLBASE', () => {
